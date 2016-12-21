bc5edb50ab6f16c2c7891a8f1c0108d3f894de40
bryn2008 <brynla.lloyd@gmai.com>
2016-12-21 15:58:50 +0000

    added primative data types

--- ByteShortInt/src/com/brynlloyd/Main.java
+++ ByteShortInt/src/com/brynlloyd/Main.java
@@ -3,6 +3,18 @@ package com.brynlloyd;
 public class Main {
 
     public static void main(String[] args) {
-	    int myValue = 10000;
+
+        // int has a width of 32
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+
+        // byte has a width of 8
+        byte myByteValue = -128;
+
+        // short has a width of 16
+        short myShortValue = 32767;
+
+        // long has a width of 64
+        long myLongValue = 9_223_372_036_854_775_807L;
     }
 }
