91f5ab71049f2b59703730bb709bfe3cae2a2673
bryn.lloyd <bryn.lloyd@metoffice.gov.uk>
2016-12-21 15:42:06 +0000

    initial commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * Created by bryn.lloyd on 21/12/2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Bryn!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThridNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThridNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+
+    }
+
+}
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by bryn.lloyd on 21/12/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bryn!");
+    }
+}
