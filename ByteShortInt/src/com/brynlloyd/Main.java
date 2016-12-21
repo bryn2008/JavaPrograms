fd846827b75634bdeb108bfda469f3a50f836cfa
bryn2008 <brynla.lloyd@gmai.com>
2016-12-21 16:07:37 +0000

    started chalenge

--- ByteShortInt/src/com/brynlloyd/Main.java
+++ ByteShortInt/src/com/brynlloyd/Main.java
@@ -7,14 +7,24 @@ public class Main {
         // int has a width of 32
         int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647;
+        int myIntValue = 2647;
+        int myTotal = (myMinValue/2);
+        System.out.println("MyTotal = " + myTotal);
 
         // byte has a width of 8
-        byte myByteValue = -128;
+        byte myMinByteValue = -128;
+        byte myByteValue = 58;
+        byte myNewByteValue = (byte) (myByteValue/2);
+        System.out.println("myByteValue = " + myByteValue / 2);
 
         // short has a width of 16
         short myShortValue = 32767;
 
         // long has a width of 64
-        long myLongValue = 9_223_372_036_854_775_807L;
+        long myMaxLongValue = 9_223_372_036_854_775_807L;
+
+
+        int total = myIntValue +  myByteValue + myShortValue;
+        long myLongValue = 50000 * total;
     }
 }
