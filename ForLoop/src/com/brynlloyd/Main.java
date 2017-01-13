6a0967d7d568806f9358efb99b256903769a9154
bryn2008 <brynla.lloyd@gmai.com>
2017-01-13 12:05:54 +0000

    added new classes

--- ForLoop/src/com/brynlloyd/Main.java
+++ ForLoop/src/com/brynlloyd/Main.java
@@ -4,9 +4,50 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("10,000 at 2% interest = " + caculateUnterest(10000.0, 2.0));
-        System.out.println("10,000 at 3% interest = " + caculateUnterest(10000.0, 3.0));
-        System.out.println("10,000 at 4% interest = " + caculateUnterest(10000.0, 4.0));
-        System.out.println("10,000 at 5% interest = " + caculateUnterest(10000.0, 5.0));
+
+        for (int i=0; i<5; i++){
+            System.out.println("Loop " + i + " hello");
+        }
+
+        for (int i=2; i<9; i++){
+            System.out.println("10,000 at "+i+"% interest = " + String.format("%.2f", caculateUnterest(10000.0, i)));
+        }
+
+        System.out.println("**************");
+
+        for (int i=9; i>1; i--){
+            System.out.println("10,000 at "+i+"% interest = " + String.format("%.2f", caculateUnterest(10000.0, i)));
+        }
+
+        int pNumCount = 0;
+        int totalNumbers = 50;
+        for (int i=10; i<totalNumbers ; i++){
+            boolean pNum = isPrime(i);
+            if (pNum){
+                pNumCount++;
+                System.out.println(i + " is a Prime number");
+                if(pNumCount == 10){
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+        System.out.println("There are " + pNumCount + " prime numbers in " + totalNumbers);
+    }
+
+    public static boolean isPrime(int n){
+
+        if (n == 1){
+            return false;
+        }
+
+        for (int i=2; i<= n/2; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static double caculateUnterest(double amount, double interestRate){
